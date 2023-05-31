13f95ec3d286e79a3365cbb524a8b2d42e1a397d
TariqMehmood1004 <116074810+TariqMehmood1004@users.noreply.github.com>
2023-06-01 01:38:48 +0500

    Hope to finished

---
+++ lib/game_over.dart
@@ -0,0 +1,41 @@
+import 'package:app/menu_card.dart';
+import 'package:app/padracing_game.dart';
+import 'package:flutter/material.dart' hide Image, Gradient;
+
+class GameOver extends StatelessWidget {
+  const GameOver(this.game, {super.key});
+
+  final PadRacingGame game;
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    return Material(
+      color: Colors.transparent,
+      child: Center(
+        child: Wrap(
+          children: [
+            MenuCard(
+              children: [
+                Text(
+                  'Player ${game.winner!.playerNumber + 1} wins!',
+                  style: textTheme.displayLarge,
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  'Time: ${game.timePassed}',
+                  style: textTheme.bodyLarge,
+                ),
+                const SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: game.reset,
+                  child: const Text('Restart'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
